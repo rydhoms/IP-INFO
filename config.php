683b36628254786b7ece00bf9860e8bc3699fe9f
Ridho Muhammad <hello@ridho.id>
2025-12-13 01:27:04 +0700

    Define API endpoints in config.php
    Added API endpoint definitions for default, IPv4, and IPv6.

--- config.php
+++ config.php
@@ -1,6 +1,13 @@
 <?php
 // config.php
 
+// API Endpoints
+// Create your own API IP here https://github.com/rydhoms/ip-api/
+// or you can use public IP service like ipfy.org
+define('API_DEFAULT', 'https://api.example.com');
+define('API_IPV4', 'https://api4.example.com');
+define('API_IPV6', 'https://api6.example.com');
+
 // IPHub API Key
 define('IPHUB_API_KEY', 'your_key_here'); // get your free key here https://iphub.info/
 
